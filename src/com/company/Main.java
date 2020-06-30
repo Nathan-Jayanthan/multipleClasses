04f43d7a6d6d8274fb5a21a74f63c26411718256
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-29 18:01:45 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// to use another class you need to create an object
+        //first class name, class object set eql to new and class name
+        //to use it use the object name dot the method name u want to use
+        tuna tunaObject = new tuna();
+        tunaObject.simpleMessage();
+    }
+}
---
+++ src/com/company/tuna.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class tuna {
+    public void simpleMessage() {
+        System.out.println("tuna class");
+    }
+}
